cf8b0f1483569260d82386285ccdec9ca3fd2da9
Santiago Saavedra <santiagosaavedra@gmail.com>
2013-04-18 19:33:34 +0200

    Now we can use `any` Solarized-based package.
    Just put the themes in the dark and light arrays.

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -5,25 +5,35 @@ import os.path
 
 class SolarizedToggle(sublime_plugin.ApplicationCommand):
 
-    def set_color_scheme(self, scheme_type, settings):
-        default_light = "Packages/Color Scheme - Default/Solarized (Light).tmTheme"
-        default_dark = "Packages/Color Scheme - Default/Solarized (Dark).tmTheme"
-        color_scheme_light = "Packages/Solarized Color Scheme/Solarized (light).tmTheme"
-        color_scheme_dark = "Packages/Solarized Color Scheme/Solarized (dark).tmTheme"
+    def set_color_scheme(self, settings):
+
+        colorscheme = {
+            "dark" : [
+                "Packages/solarized-sublimetext2/Solarized (dark).tmTheme",
+                "Packages/Solarized Color Scheme/Solarized (dark).tmTheme",
+                "Packages/Color Scheme - Default/Solarized (Dark).tmTheme"
+            ],
+            "light" : [
+                "Packages/solarized-sublimetext2/Solarized (light).tmTheme",
+                "Packages/Solarized Color Scheme/Solarized (light).tmTheme",
+                "Packages/Color Scheme - Default/Solarized (Light).tmTheme"
+            ]
+        }
 
         current_scheme = settings.get("color_scheme")
         new_scheme = "light" if "dark" in current_scheme.lower() else "dark"
 
-        if scheme_type == "color":
-            if new_scheme == "light":
-                settings.set("color_scheme", color_scheme_light)
-            else:
-                settings.set("color_scheme", color_scheme_dark)
-        else:
-            if new_scheme == "light":
-                settings.set("color_scheme", default_light)
-            else:
-                settings.set("color_scheme", default_dark)
+        paths = colorscheme[new_scheme]
+
+        for path in paths:
+            real_path = sublime.packages_path() + path.replace('Packages', '')
+            if os.path.exists(real_path):
+                # Uncomment for debugging
+                # print "Setting theme to", real_path
+                settings.set("color_scheme", path)
+                return
+
+        print "[SolarizedToggle]::[WARN] Could find no path to Solarized.."
 
     def run(self, **args):
         settingsFile = "Preferences.sublime-settings"
@@ -33,9 +43,6 @@ class SolarizedToggle(sublime_plugin.ApplicationCommand):
         sublime3_path = (sublime.installed_packages_path() +
                             "/Solarized Color Scheme.sublime-package")
 
-        if os.path.exists(sublime2_path) or os.path.exists(sublime3_path):
-            self.set_color_scheme("color", settings)
-        else:
-            self.set_color_scheme("default", settings)
+        self.set_color_scheme(settings)
 
         sublime.save_settings(settingsFile)
