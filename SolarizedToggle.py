a9c2e58ff292ae0d3652884621eea055aaebe15b
Santiago Saavedra <santiagosaavedra@gmail.com>
2013-04-20 15:40:51 +0200

    Adds color schemes to the config file.

--- SolarizedToggle.py
+++ SolarizedToggle.py
@@ -1,44 +1,81 @@
 import sublime
 import sublime_plugin
+import os.path
 
 
 class SolarizedToggle(object):
-    def plugin_loaded_setup(self):
-        self.global_settings = sublime.load_settings('Preferences.sublime-settings')
+    def __init__(self):
+        self.global_settings_file = 'Preferences.sublime-settings'
+        self.global_settings = sublime.load_settings(self.global_settings_file)
         self.plugin_settings_file = 'SolarizedToggle.sublime-settings'
         self.plugin_settings = sublime.load_settings(self.plugin_settings_file)
         self.state = self.plugin_settings.get('current_state')
+        self.theme = self.global_settings.get('color_scheme')
+        self.state = self.get_group(self.theme)
+        print self.global_settings.get('color_scheme')
 
-    def _set(self, setting, state):
-        new = self.plugin_settings.get("{}_{}".format(setting, state))
-        self.global_settings.set(setting, new)
+        if not self.plugin_settings.has('color_schemes'):
+            self.plugin_settings.set('color_schemes', {
+                "dark": [
+                    "Packages/solarized-sublimetext3/Solarized (dark).tmTheme",
+                    "Packages/solarized-sublimetext2/Solarized (dark).tmTheme",
+                    "Packages/Solarized Color Scheme/Solarized (dark).tmTheme",
+                    "Packages/Color Scheme - Default/Solarized (Dark).tmTheme"
+                ],
+                "light": [
+                    "Packages/solarized-sublimetext3/Solarized (light).tmTheme",
+                    "Packages/solarized-sublimetext2/Solarized (light).tmTheme",
+                    "Packages/Solarized Color Scheme/Solarized (light).tmTheme",
+                    "Packages/Color Scheme - Default/Solarized (Light).tmTheme"
+                ]})
+
+    def get_group(self, scheme):
+        if scheme is None:
+            print "No current scheme"
+            return None
+        return "light" if "light" in scheme.lower() else "dark"
+
+    def get_next_group(self, scheme):
+        if scheme is None:
+            print "No current scheme"
+            return None
+        return "light" if "dark" in scheme.lower() else "dark"
 
     def _flip_state(self):
-        if self.state == 'dark':
-            self.state = 'light'
-        else:
-            self.state = 'dark'
+        self.state = self.get_next_group(self.state)
         self.plugin_settings.set('current_state', self.state)
         sublime.save_settings(self.plugin_settings_file)
 
+    def set_state(self, value):
+        possible = self.plugin_settings.get('color_schemes')[value]
+        for each in possible:
+            path = sublime.packages_path() + each.replace('Packages', '')
+            if os.path.exists(path):
+                print "Setting theme to ", path
+            self.global_settings.set('color_scheme', each)
+            self.theme = each
+            sublime.save_settings(self.global_settings_file)
+            return each
+
     def flip(self):
         self._flip_state()
-        self._set('color_scheme', self.state)
-        if self.plugin_settings.get("flip_theme"):
-            self._set('theme', self.state)
-        self.update_view()
+        theme = self.set_state(self.state)
+        # if self.plugin_settings.get("flip_theme"):
+        #     self._set('theme', self.state)
+        self.update_views()
 
-    def update_view(self, view=None):
-        desired_scheme = (self.plugin_settings
-                          .get("color_scheme_{}"
-                          .format(self.state)))
-        (sublime.active_window().active_view()
-         .settings().set('color_scheme', desired_scheme))
+    def update_views(self):
+        for window in sublime.windows()
+            for view in window.views():
+                view.settings().set('color_scheme', self.theme)
+        # print "Setting current window theme to ", self.theme
+        # (sublime.active_window().active_view()
+        #  .settings().set('color_scheme', self.theme))
 
 
-class SolarizedToggleListener(sublime_plugin.EventListener):
-    def on_activated(self, view):
-        _flipper.update_view(view)
+# class SolarizedToggleListener(sublime_plugin.EventListener):
+#     def on_activated(self, view):
+#         _flipper.update_views(view)
 
 
 class SolarizedToggleCommand(sublime_plugin.ApplicationCommand):
@@ -51,3 +88,5 @@ _flipper = SolarizedToggle()
 
 def plugin_loaded():
     _flipper.plugin_loaded_setup()
+
+# plugin_loaded()
